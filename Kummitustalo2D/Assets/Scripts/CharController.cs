4b187b4352738643f5efe53cdc189ee273338070
Timo Sormunen <timsorm@gmail.com>
2017-10-02 00:03:43 +0300

    add script for basic character controls

--- Kummitustalo2D/Assets/Scripts/CharController.cs
+++ Kummitustalo2D/Assets/Scripts/CharController.cs
@@ -2,15 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour {
+public class CharController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public float speed = 5f;
+    float someScale;
+
+	//public Rigidbody2D playerRB;
+
+	void Start()
+	{
+		someScale = transform.localScale.x;
+		//playerRB = GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Update()
+	{
+
 	}
+
+	void FixedUpdate()
+	{
+
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+        transform.Translate(moveHorizontal * Time.deltaTime * speed, moveVertical * Time.deltaTime * speed, 0);
+
+		if (moveHorizontal > 0)
+		{
+			transform.localScale = new Vector2(-someScale, transform.localScale.y);
+		}
+		if (moveHorizontal < 0)
+		{
+			transform.localScale = new Vector2(someScale, transform.localScale.y);
+		}
+
+
+
+		//float moveHorizontal = Input.GetAxis("Horizontal");
+		//float moveVertical = Input.GetAxis("Vertical");
+
+		//Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		//playerRB.AddForce(movement * speed);
+
+		//if (playerRB.velocity.x >= 0) {
+		//    transform.localScale = new Vector2(someScale, transform.localScale.y);
+		//} else {
+		//    transform.localScale = new Vector2(-someScale, transform.localScale.y);
+		//}
+
+	}
+
 }
