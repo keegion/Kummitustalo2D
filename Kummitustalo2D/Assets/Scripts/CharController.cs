fe4960b0fa637a62a71b818a058a766d794c707d
Timo Sormunen <timsorm@gmail.com>
2017-10-02 22:59:59 +0300

    cleaned up old shit from charController.cs

--- Kummitustalo2D/Assets/Scripts/CharController.cs
+++ Kummitustalo2D/Assets/Scripts/CharController.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class CharController : MonoBehaviour {
 
 	public float speed = 5f;
-	//float someScale;
+
 	bool facingRight;
 	float input_x;
 	float input_y;
 
 	void Start()
 	{
-		//someScale = transform.localScale.x;
+
 	}
 
 	void Update()
@@ -22,38 +22,23 @@ public class CharController : MonoBehaviour {
 
 	void FixedUpdate()
 	{
-
 		input_x = Input.GetAxis("Horizontal");
 		input_y = Input.GetAxis("Vertical");
 
 		transform.Translate(input_x * Time.deltaTime * speed, input_y * Time.deltaTime * speed, 0);
 
-		//if (input_x > 0)
-		//{
-		//	transform.localScale = new Vector2(-someScale, transform.localScale.y);
-		//}
-		//if (input_x < 0)
-		//{
-		//	transform.localScale = new Vector2(someScale, transform.localScale.y);
-		//}
-
-
 		if (input_x < 0 && facingRight)
 		{
 			Flip();
-
 		}
 		else if (input_x > 0 && !facingRight)
 		{
 			Flip();
-
 		}
-
 	}
 
 	void Flip()
 	{
-
 		facingRight = !facingRight;
 		transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
 	}
