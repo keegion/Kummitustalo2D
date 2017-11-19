898f2884749a06c06ea143f96f69ef0682bd4482
Mihkel <mihkeltanel@gmail.com>
2017-11-19 15:14:57 +0200

    Added finish panel/ clear message

--- Kummitustalo2D/Assets/Scripts/PortalSummon.cs
+++ Kummitustalo2D/Assets/Scripts/PortalSummon.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PortalSummon : MonoBehaviour {
     public int MaxShards;
     //private GameObject gm;
     public GameObject portal;
+    public int levels;
+    public GameObject FinishMenu;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         //gm = GameObject.Find("GameManager(Clone)");
     }
 	
@@ -24,10 +28,34 @@ public class PortalSummon : MonoBehaviour {
 
         if (MaxShards <= currentShards)
         {
+
             Debug.Log("Summon Portal");
+            if(CheckifLastLevel())
+            {
+                OpenFinishPanel();
+            }
+            else
             Instantiate(portal, transform.position, transform.rotation);
+           
         }
     }
+    public bool CheckifLastLevel()
+    {
+       
+        if (SceneManager.GetActiveScene().name == "level_" + levels)
+            return true;
+            
+      
+        else
+            return false;
+    }
+    void OpenFinishPanel()
+    {
+        FinishMenu.SetActive(true);
+        Time.timeScale = 0;
+
+    }
+
 
 
 }
