1a30bc287d73ce9c3e967178dbaea19aa3d4fcfb
Timo Sormunen <timsorm@gmail.com>
2017-11-22 15:42:38 +0200

    clean up enemybullet.cs

--- Kummitustalo2D/Assets/Scripts/EnemyBullet.cs
+++ Kummitustalo2D/Assets/Scripts/EnemyBullet.cs
@@ -7,7 +7,7 @@ public class EnemyBullet : MonoBehaviour {
     public float speed;
     public GameObject particle;
 	Rigidbody2D rb;
-	public bool isBoomerang;
+	//public bool isBoomerang;
 	int direction = 1;
 
 	void Start()
@@ -20,36 +20,12 @@ public class EnemyBullet : MonoBehaviour {
 			direction = -1;
 		}
 
-		if (isBoomerang)
-		{
-			rb.AddForce(new Vector2(direction * speed, 0.15f * speed), ForceMode2D.Impulse);
-		}
-		else 
-		{
-			rb.AddForce(new Vector2(direction * speed, 0), ForceMode2D.Impulse);
-		}
+		rb.AddForce(new Vector2(direction * speed, 0), ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-		if (isBoomerang)
-		{
-			if ( (rb.velocity.x > 0 && direction == 1) || (rb.velocity.x < 0 && direction == -1) )
-			{
-				rb.AddForce(new Vector2(-1 * direction * speed * Time.deltaTime, -0.3f * speed * Time.deltaTime), ForceMode2D.Impulse);
-			}
-			else if ( (rb.velocity.x < 0 && direction == 1) || (rb.velocity.x > 0 && direction == -1) )
-			{
-				rb.AddForce(new Vector2(-1 * direction * speed * Time.deltaTime, 0.3f * speed * Time.deltaTime), ForceMode2D.Impulse);
-			}
-
-			rb.transform.Rotate(0, 0, 7);
-			Destroy(gameObject, 2f);
-		}
-
-		// pitäiskö palautuessaan jäädä heppamiehen käteen tai "päähän"? Lerpillä esim. loppumatka?
-
 		// vanha tapa velocityllä:
         //GetComponent<Rigidbody2D>().velocity = new Vector2(speed, GetComponent<Rigidbody2D>().velocity.y);
     }
