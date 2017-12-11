e2b35a87bc1bf6e772f2eeaeb76801ff4d2e1c7d
Timo Sormunen <timsorm@gmail.com>
2017-12-11 19:44:31 +0200

    fix for resetting player lives etc. if new game started after returning to main menu from pause menu

---
+++ Kummitustalo2D/Assets/Scripts/DestroyGameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyGameManager : MonoBehaviour {
+
+	private GameObject GameManager;
+
+	void Start()
+	{
+		GameManager = GameObject.Find("GameManager(Clone)");
+		// reset all player stuff
+		Destroy(GameManager);
+	}
+}
