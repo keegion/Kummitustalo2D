fe2372c6a9ec7bc65b3a7360ffd5b45396002655
Timo Sormunen <timsorm@gmail.com>
2017-12-13 15:45:42 +0200

    add story and art panels

---
+++ Kummitustalo2D/Assets/Scripts/StoryPanel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryPanel : MonoBehaviour {
+
+	public GameObject storyPanel;
+	public GameObject artPanel;
+
+	public void ShowStoryPanel()
+	{
+		storyPanel.SetActive(true);
+		HideArtPanel();
+	}
+
+	public void ShowArtPanel()
+	{
+		artPanel.SetActive(true);
+		HideStoryPanel();
+	}
+
+	public void HideStoryPanel()
+	{
+		storyPanel.SetActive(false);
+	}
+
+	public void HideArtPanel()
+	{
+		artPanel.SetActive(false);
+	}
+
+}
