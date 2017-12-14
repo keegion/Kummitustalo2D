1f0be6e5583f9b4fe0cd46c72f8b7246956c0f3e
Timo Sormunen <timsorm@gmail.com>
2017-12-14 23:13:22 +0200

    update game over scene

---
+++ Kummitustalo2D/Assets/Scripts/GameOver.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+    private GameObject GameManager;
+
+    void Start () {
+		GameManager = GameObject.Find("GameManager(Clone)");
+        // reset all player stuff
+		Destroy(GameManager);
+    }
+	
+	void Update () {
+		//if (Input.anyKey) {
+  //          SceneManager.LoadScene("MainMenu");
+		//}
+	}
+
+	public void MainMenu()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+	public void Exit()
+	{
+		Application.Quit();
+	}
+}
